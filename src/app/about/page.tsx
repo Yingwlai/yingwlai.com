1ecc9ceeb8440ae0143fec53cd04b699e37ca4bd
Ying <Yingwlai@hotmail.com>
2024-12-28 22:43:47 +0000

    Prevent page prerendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,13 @@
+export const dynamic = 'force-dynamic';
+
 import Summary from './components/Summary/Summary';
 
 import Layout from '../components/Layout/Layout';
 import { BASE_URL_PATH } from '../constants/navigation';
 
 const AboutPage = async () => {
-  const { navigationItems } = await fetch(`${BASE_URL_PATH}/api/app-navigation`).then((response) => response.json());
+  const { navigationItems } = await fetch(`${BASE_URL_PATH}/api/app-navigation`)
+    .then((response) => response.json());
 
   return (
     <Layout navigationItems={navigationItems}>
